57656716a15d203aac3b87b11332756d09774fce
Shreyas Tallamraju <shreyastallamraju@gmail.com>
2020-05-22 21:07:17 -0500

    version 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import MuiAlert from '@material-ui/lab/Alert';
 import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import SettingsIcon from '@material-ui/icons/Settings';
+import HelpIcon from '@material-ui/icons/Help';
 import Drawer from '@material-ui/core/Drawer';
 import TextField from '@material-ui/core/TextField';
 import List from '@material-ui/core/List';
@@ -16,24 +17,44 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import MessageIcon from '@material-ui/icons/Message';
 import Geolocation from 'react-geolocation';
-import PanicButtonImg from './PanicButton.png';
-import PanicButtonPressedImg from './PanicButtonPressed.png';
+import Fab from '@material-ui/core/Fab';
+import red from '@material-ui/core/colors/red';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Snackbar from '@material-ui/core/Snackbar';
+import Divider from '@material-ui/core/Divider';
+
+//Alert from Material-UI Labs
+const Alert = (props) => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: '#F44335',
+    },
+    primary: red,
+    secondary: {
+      main: '#F44335',
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    height: '100%',
     textAlign: 'center',
+    backgroundColor: '#F44335',
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+
   },
   title: {
     flexGrow: 1,
-    marginRight: theme.spacing(5),
   },
   callInput: {
     width: "80%",
-    marginTop: "20px",
     textAlign: "center",
   },
   alertText: {
@@ -43,9 +64,39 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 'bolder',
     marginTop: '10px',
   },
-  drawer: {
-    margin: '20px',
-    padding: '20px',
+  helpDrawer: {
+    width: '100%',
+    height: '100%',
+    padding: '40px',
+    backgroundColor: '#F44335',
+  },
+  settingsDrawer: {
+    width: '100%',
+    height: '100%',
+    padding: '40px',
+    backgroundColor: '#F44335',
+  },
+  appBar: {
+
+  },
+  panicButton: {
+    marginTop: '70%',
+    width: '300px',
+    height: '300px',
+    fontSize: '190px',
+  },
+  panicButtonText: {
+    marginTop: '-30px',
+  },
+  hidden: {
+    visibility: 'hidden',
+  },
+  divider: {
+    marginTop: '20px',
+    marginBottom: '20px',
+  },
+  links: {
+    textDecoration: 'none',
   },
 }));
 
@@ -54,6 +105,7 @@ const App = () => {
   var localStorage = require('local-storage');
   //local storage keys: wsh-call, wsh-text
   const [settingsOpen, setSettingsOpen] = useState(false);
+  const [helpOpen, setHelpOpen] = useState(false);
   const [callNumber, setCallNumber] = useState("911")
   const [panicBtnPressed, setPanicBtnPressed] = useState(false);
   const [textNumbers, setTextNumbers] = useState(["No Texting Numbers Stored!"]);
@@ -71,18 +123,85 @@ const App = () => {
     setPanicBtnPressed(true);
     window.location.href = "tel://" + callNumber;
   }
+  const handleCloseSnackbar = (e, r) => {
+    if (r === 'clickaway') {
+      return;
+    }
+    setPanicBtnPressed(false);
+  }
   return (
-    <div className={classes.root}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      {/*Help Drawer*/}
+      <Drawer
+        anchor="left"
+        className={classes.helpDrawer}
+        open={helpOpen}
+        onClose={() => {setHelpOpen(false)}}
+      >
+        {/*Help Content*/}
+        <div className={classes.helpDrawer}>
+        <Grid 
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          <IconButton onClick={() => {setHelpOpen(false)}}>
+            <ArrowBackIosIcon />
+          </IconButton>
+          <Typography variant="h3">
+            Welcome to WalkSafe &#8962;
+          </Typography>
+        </Grid>
+
+        <Divider className={classes.divider} />
+
+        <Typography variant="body1">
+          WalkSafe &#8962; is a personal safety app that calls and texts your emergency contacts with the click of just one button! <br />
+          When the panic button is clicked, one contact of your choice will be called from your phone. <br />
+          In addition, up to 10 other numbers will be texted an SOS message automatically from our service about you. <br />
+          To edit the number to call/text automatically, click on the settings gear (<SettingsIcon />) on the top right of the screen. <br />
+        </Typography>
+
+        {/*Help - Footer*/}
+        <Divider className={classes.divider} />
+        <Typography variant="body2">
+          Made with &hearts; by Shrey Ravi<br />
+          WalkSafe &#8962; is not a replacement for law enforcement/safety<br />
+          <a className={classes.links} href="https://www.shreyravi.com/contact.html">Contact</a> | <a className={classes.links} href="#">Legal</a> | <a className={classes.links} href="https://github.com/ShreyRavi/walk-safe-home">GitHub</a> | <a className={classes.links} href="https://github.com/ShreyRavi/walk-safe-home/commits/master">Version 2</a>
+        </Typography>
+
+        </div>
+      </Drawer>
 
       {/*Setting Drawer*/}
       <Drawer
-        anchor="left"
-        className={classes.drawer}
+        anchor="right"
+        className={classes.settingsDrawer}
         open={settingsOpen}
         onClose={() => {setSettingsOpen(false)}}
       >
+        <div className={classes.settingsDrawer}>
+        <Grid 
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          <IconButton onClick={() => {setSettingsOpen(false)}}>
+            <ArrowBackIosIcon />
+          </IconButton>
+          <Typography variant="h3">
+            WalkSafe &#8962; Settings
+          </Typography>
+        </Grid>
+
+        <Divider className={classes.divider} />
+
         {/*Settings - CallInput*/}
-        <Typography className={classes.directions} variant="body2">enter number to call</Typography>
+        <Typography className={classes.directions} variant="body2">Enter number to call:<br /></Typography>
         <TextField 
         className={classes.callInput} 
         onInput={(e) => {if(/^\d+$/.test(e.target.value)){setCallNumber(e.target.value); localStorage.set("wsh-call",e.target.value);}}} 
@@ -95,8 +214,8 @@ const App = () => {
         </TextField>
 
         {/*Settings - TextInput*/}
-        <hr />
-        <Typography className={classes.directions} variant="body2">enter number(s) to text</Typography>
+        <Divider className={classes.divider} />
+        <Typography className={classes.directions} variant="body2">Enter number(s) to text:<br /></Typography>
         <TextField 
         className={classes.callInput} 
         onInput={(e) => {setTextNumbers(e.target.value.split(",")); localStorage.set("wsh-text",e.target.value);}} 
@@ -107,70 +226,11 @@ const App = () => {
         inputProps={{style: { textAlign: 'center' }}}
         >
         </TextField>
-        <Typography variant="body2">seperate each number with a comma<br />e.g. "4408379929,7739939992"</Typography>
-      </Drawer>
+        <Typography variant="body2">seperate each number with a comma<br />e.g. "4408379929,7739939992"<br /></Typography>
 
-      {/*Header*/}
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => {setSettingsOpen(true)}}>
-            <SettingsIcon />
-          </IconButton>
-          <Typography variant="h4" className={classes.title}>
-            <center> WalkSafe &#8962; </center>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      >
-        {/*CallInput*/}
-        <Typography className={classes.directions} variant="body2">click red button to call your emergency contact below</Typography>
-        <TextField 
-        className={classes.callInput} 
-        onInput={(e) => {if(/^\d+$/.test(e.target.value)){setCallNumber(e.target.value); localStorage.set("wsh-call",e.target.value);}}} 
-        id="CallInput" 
-        label="Emergency Number to Call &#9742;" 
-        variant="outlined" 
-        value={callNumber}
-        inputProps={{style: { textAlign: 'center' }}}
-        >
-        </TextField>
-        <Typography variant="body2">the number you enter will be autosaved on your phone</Typography>
-
-        {/*GeoLocation*/}
-        <hr />
-        <Geolocation
-          render={({
-            fetchingPosition,
-            position: { coords: { latitude, longitude } = {} } = {},
-            error,
-            getCurrentPosition
-          }) =>
-            <div>
-              {/*PanicButton*/}
-              <Button onClick={() => {getCurrentPosition(); handlePanic()}} ><img src={panicBtnPressed ? PanicButtonPressedImg : PanicButtonImg} alt="Panic Button" /></Button> <br />
-              {panicBtnPressed ? 
-                <Typography className={classes.alertText} variant="body2">panic button pressed!<br />help texts sent to below numbers</Typography>
-              : <div></div>}
-              {error &&
-                <Typography className={classes.alertText} variant="body2">
-                  {error.message}
-                </Typography>}
-              <Typography variant="body2">
-                LATITUDE: {isNaN(parseFloat(latitude)) ? <div></div> : parseFloat(latitude).toFixed(4)}<br />
-                LONGITUDE: {isNaN(parseFloat(longitude)) ? <div></div> : parseFloat(longitude).toFixed(4)}
-              </Typography>
-            </div>}
-        />
-
-        {/*TextsInput*/}
-        <hr />
-        <Typography variant="body2">numbers texted when the panic button pressed:</Typography>
+        {/*Settings - TextsShow*/}
+        <Divider className={classes.divider} />
+        <Typography variant="body2">Numbers texted when the panic button pressed:</Typography>
         <List dense={true}>
           {textNumbers.map(e => 
             <ListItem>
@@ -181,19 +241,75 @@ const App = () => {
           </ListItem>
           )}
         </List>
-        <Typography variant="body2">to edit, click the settings gear icon on the top-left</Typography>
 
-        {/*Footer*/}
-        <hr />
+        {/*Settings - Footer*/}
+        <Divider className={classes.divider} />
         <Typography variant="body2">
-          made with &hearts; by Shrey Ravi for free<br />
+          Made with &hearts; by Shrey Ravi<br />
           WalkSafe &#8962; is not a replacement for law enforcement/safety<br />
-          <a href="https://www.shreyravi.com/contact.html">Contact</a> | <a href="#">Legal</a> | <a href="https://github.com/ShreyRavi/walk-safe-home">GitHub</a> | <a href="https://github.com/ShreyRavi/walk-safe-home/commit/cdab0072a54bd6e8f479ee4489367fb4d9f66eed">v1.0</a>
+          <a href="https://www.shreyravi.com/contact.html">Contact</a> | <a href="#">Legal</a> | <a href="https://github.com/ShreyRavi/walk-safe-home">GitHub</a> | <a href="https://github.com/ShreyRavi/walk-safe-home/commits/master">Version 2</a>
         </Typography>
 
+        </div>
+      </Drawer>
+
+      {/*Header*/}
+      <AppBar className={classes.appBar} position="fixed" color="primary">
+        <Toolbar>
+          <IconButton edge="start" className={classes.helpButton} color="inherit" aria-label="menu" onClick={() => {setHelpOpen(true)}}>
+            <HelpIcon />
+          </IconButton>
+          <Typography variant="h4" className={classes.title}>
+            <center> WalkSafe &#8962; </center>
+          </Typography>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => {setSettingsOpen(true)}}>
+            <SettingsIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        className={classes.root}
+      >
+
+        {/*GeoLocation + Panic Button*/}
+        <Geolocation
+          render={({
+            fetchingPosition,
+            position: { coords: { latitude, longitude } = {} } = {},
+            error,
+            getCurrentPosition
+          }) =>
+            <div>
+              {/*PanicButton*/}
+              <Fab className={classes.panicButton} size="large" variant="round" color="primary" onClick={() => {getCurrentPosition(); handlePanic()}}><div className={classes.panicButtonText}>&#8962;</div></Fab>
+              {panicBtnPressed ? 
+                <Snackbar open={panicBtnPressed} autoHideDuration={15000} onClose={handleCloseSnackbar}>
+                  <Alert onClose={handleCloseSnackbar} severity="success">
+                    Panic button pressed, help texts sent to selected numbers!
+                  </Alert>
+                </Snackbar>
+              : <div></div>}
+              {error && <Snackbar open={panicBtnPressed} onClose={handleCloseSnackbar} autoHideDuration={15000}> 
+                <Alert onClose={handleCloseSnackbar} severity="error">
+                  {error.message}
+                </Alert>
+              </Snackbar>}
+              <Typography className={classes.hidden} variant="body2">
+                LATITUDE:  {parseFloat(latitude).toFixed(4)}<br />
+                LONGITUDE: {parseFloat(longitude).toFixed(4)}
+              </Typography>
+            </div>}
+        />
+        {/*<br /><br />
+        <Typography variant="body2">made by Shrey Ravi &hearts; stay safe</Typography>*/}
       
       </Grid>
-    </div>
+    </ThemeProvider>
   );
 }
 
